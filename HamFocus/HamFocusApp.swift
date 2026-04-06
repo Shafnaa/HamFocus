5b4081a2cc7f672b1b935467a38f310dea4008cd
Saujana Shafi <hello@saujanashafi.com>
2026-04-06 09:46:39 +0700

    initial git setup

---
+++ HamFocus/HamFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HamFocusApp.swift
+//  HamFocus
+//
+//  Created by Saujana Shafi on 06/04/26.
+//
+
+import SwiftUI
+
+@main
+struct HamFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
