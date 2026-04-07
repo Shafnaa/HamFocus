731b6526bd41681c37595bdfe755a1bf66f15222
Saujana Shafi <hello@saujanashafi.com>
2026-04-07 14:29:59 +0700

    feat(commons): Task List View

--- HamFocus/Views/Commons/TaskListView.swift
+++ HamFocus/Views/Commons/TaskListView.swift
@@ -9,5 +9,52 @@ import Foundation
 import SwiftUI
 
 struct TaskListView: View {
-    var body: some View {}
+    var taskList: [Task]
+
+    var body: some View {
+        List {
+            ForEach(taskList) { task in
+                TaskListItemView(
+                    taskName: task.title,
+                    deadline: Date(timeIntervalSince1970: task.dueAt),
+                    isDone: task.isCompleted,
+                    onToggle: {}
+                )
+            }
+        }
+        .listStyle(.plain)
+        .scrollContentBackground(.hidden)
+        .clipShape(RoundedRectangle(cornerRadius: 24))
+    }
+}
+
+struct TaskListViewPreview: PreviewProvider {
+    static var taskList: [Task] = [
+        Task(
+            title: "Task",
+            dueAt: Date().timeIntervalSince1970,
+            duration: 6000,
+            importance: .high,
+        ),
+        Task(
+            title: "Task",
+            dueAt: Date().timeIntervalSince1970,
+            duration: 6000,
+            importance: .high,
+        ),
+        Task(
+            title: "Task",
+            dueAt: Date().timeIntervalSince1970,
+            duration: 6000,
+            importance: .high,
+        ),
+    ]
+
+    @ViewBuilder static var previews: some View {
+        TaskListView(taskList: taskList)
+    }
+}
+
+#Preview {
+    TaskListViewPreview.previews
 }
