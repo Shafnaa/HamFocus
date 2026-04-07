59ab11dfe7da8d69fb127535c70d3818c18a348d
estpoz <heidymuditas@gmail.com>
2026-04-07 11:51:04 +0700

    feat: add TaskCarouselItemView (for sliders di home)

--- HamFocus/Views/Commons/TaskCarouselItemView.swift
+++ HamFocus/Views/Commons/TaskCarouselItemView.swift
@@ -9,6 +9,30 @@ import Foundation
 import SwiftUI
 
 struct TaskCarouselItemView: View {
+    let task: Task
 
-    var body: some View {}
+    var body: some View {
+        VStack(spacing: 12) {
+            Image(task.priority.iconName)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 180)
+
+            Text(task.title)
+                .font(.title3)
+                .fontWeight(.semibold)
+                .multilineTextAlignment(.center)
+        }
+    }
+}
+
+#Preview {
+    TaskCarouselItemView(
+        task: Task(
+            title: "Kerjain ADA",
+            dueAt: Date().timeIntervalSince1970,
+            duration: 60,
+            importance: .high
+        )
+    )
 }
