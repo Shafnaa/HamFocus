cbcf1d8624c45ae77ead3b41d0463579a5db05b7
matthewslesmana <matthewslesmana@gmail.com>
2026-04-08 10:05:18 +0700

    fix: Space from edges

--- HamFocus/Views/Commons/TextFieldView.swift
+++ HamFocus/Views/Commons/TextFieldView.swift
@@ -9,15 +9,15 @@ import Foundation
 import SwiftUI
 
 struct MyTextFieldView: View {
-    @State private var text = "Type Here"
+    @State private var text = "Insert your name here"
 
     var body: some View {
         VStack {
             TextField("Enter text here...", text: $text)
-                .padding() // makes it thicker
+                .padding(15) // makes it thicker
                 .background(Color.white.opacity(0.2)) // soft background
                 .cornerRadius(25) // more rounded (pill shape)
-                .padding(.horizontal) // space from screen edge
+                .padding(.horizontal,40) // space from screen edge
         }
     }
 }
