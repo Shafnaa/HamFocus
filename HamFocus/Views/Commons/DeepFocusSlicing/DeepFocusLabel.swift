666968e3696a515b0a74090f5fcb3518462e6f99
matthewslesmana <matthewslesmana@gmail.com>
2026-04-09 08:44:19 +0700

    feat: Slicing Deep Focus Folder

---
+++ HamFocus/Views/Commons/DeepFocusSlicing/DeepFocusLabel.swift
@@ -0,0 +1,18 @@
+//
+//  DeepFocusView.swift
+//  HamFocus
+//
+//  Created by Matthew Sebastian Lesmana on 09/04/26.
+//
+
+import SwiftUI
+
+struct DeepFocusView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeepFocusView()
+}
