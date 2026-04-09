3beb5cff2da6a0d11450ee5886fa712d85548b72
estpoz <heidymuditas@gmail.com>
2026-04-09 08:47:39 +0700

    fix: struct name

--- HamFocus/Views/Commons/DeepFocusSlicing/DeepFocusLabel.swift
+++ HamFocus/Views/Commons/DeepFocusSlicing/DeepFocusLabel.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct DeepFocusView: View {
+struct DeepFocusLabel: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
