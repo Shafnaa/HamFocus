f4240158215abff32820e1f4663f5823ac313b1d
raff melvern <raffsg23@gmail.com>
2026-04-09 09:32:39 +0700

    feat : alert button

---
+++ HamFocus/Views/Commons/DeepFocusSlicing/DeepFocusAlert.swift
@@ -0,0 +1,18 @@
+//
+//  DeepFocusAlert.swift
+//  HamFocus
+//
+//  Created by Raff Melvern Surya Gunawan on 09/04/26.
+//
+
+import SwiftUI
+
+struct DeepFocusAlert: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeepFocusAlert()
+}
