bf2854d82deadc5b691a42e349e516d1e5105a90
raff melvern <raffsg23@gmail.com>
2026-04-13 09:54:51 +0700

    Feat: Tampilan Onboarding

--- HamFocus/Views/Screens/Onboardingview.swift
+++ HamFocus/Views/Screens/Onboardingview.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct Onboardingview: View {
+    @State private var name = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack(spacing: 0) {
+        // Title What's your Name
+                Text("What's Your Name ?")
+                    .font(.largeTitle.bold())
+        
+        // Gambar Hamster gendut
+                Image("HamsterLayDown")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250, height: 250)
+           
+        // Textfill untuk isi nama
+                MyTextFieldView()
+                    .textFieldStyle(.roundedBorder)
+                    .frame(width: 450, height: 80)
+                    .padding(.vertical, 10)
+               
+        // Button untuk OK
+                ActionButton(title: "OK") {
+                }
+                .frame(width: 75, height: 15)
+            }
+            .padding()
+        }
     }
 }
 
