e8f71629b177bd697b3600b387f522960f38e17b
matthewslesmana <matthewslesmana@gmail.com>
2026-04-13 09:59:57 +0700

    fix: SplashScreenView added animation when start

--- HamFocus/Views/Screens/SplashScreenView.swift
+++ HamFocus/Views/Screens/SplashScreenView.swift
@@ -8,7 +8,10 @@
 import SwiftUI
 
 struct SplashScreenView: View {
-    @State private var hamsterOffset: CGFloat = 300
+    @State private var logoOpacity: CGFloat = 0
+    @State private var logoOffset: CGFloat = -20
+    @State private var subheaderOpacity: CGFloat = 0
+    @State private var hamsterOffset: CGFloat = 500
     
     var body: some View {
         ZStack {
@@ -22,18 +25,21 @@ struct SplashScreenView: View {
                 Spacer()
                 
                 // Logo PNG in the middle
-                Image("Logo")
+                Image("LogoNew")
                     .resizable()
                     .scaledToFit()
-                    .frame(width: 320)
-                    .offset(y:140)
+                    .frame(width: 340)
+                    .opacity(logoOpacity)
+                    .offset(y:logoOffset)
+                    .frame(maxWidth: .infinity)
                 
                 // Subheader PNG just below logo
-                Image("subheader")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 180)
-                    .padding(.top, 8)
+                Text("Stay on the wheel !")
+                    .font(.custom("YourFontName", size: 23).weight(.black))
+                    .bold()
+                    .foregroundColor(.white)
+                    .opacity(subheaderOpacity)
+                    .padding(.top, 4)
                 
                 Spacer()
                 
@@ -41,13 +47,24 @@ struct SplashScreenView: View {
                 Image("HamsterIdle")
                     .resizable()
                     .scaledToFit()
-                    .frame(width: 600)
+                    .frame(width: 670)
                     .offset(y: hamsterOffset) // moves based on state
+                    .frame(maxWidth: .infinity)
             }
         }
         .onAppear {
-            withAnimation(.spring(duration: 0.8)) {
-                hamsterOffset = 60 // slides up into view
+            // Logo appears first
+            withAnimation(.easeOut(duration: 0.6)) {
+                logoOpacity = 1
+                logoOffset = 0
+            }
+
+            // Subheader appears shortly after
+            withAnimation(.easeOut(duration: 0.5).delay(0.4)) {
+                subheaderOpacity = 1
+                        }
+            withAnimation(.spring(duration: 1).delay(0.9)) {
+                hamsterOffset = 150 // slides up into view
             }
         }
     }
