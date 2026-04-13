5e8bd0e90f01d81bb91aa2c44fd66316bdf8a2fb
matthewslesmana <matthewslesmana@gmail.com>
2026-04-13 09:23:03 +0700

    fix: SplashScreenView Fixed

--- HamFocus/Views/Screens/SplashScreenView.swift
+++ HamFocus/Views/Screens/SplashScreenView.swift
@@ -8,11 +8,51 @@
 import SwiftUI
 
 struct SplashScreenView: View {
+    @State private var hamsterOffset: CGFloat = 300
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // Background color or image
+            Image("BACKGROUND")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                // Logo PNG in the middle
+                Image("Logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 320)
+                    .offset(y:140)
+                
+                // Subheader PNG just below logo
+                Image("subheader")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 180)
+                    .padding(.top, 8)
+                
+                Spacer()
+                
+                // Hamster peeping from the bottom
+                Image("HamsterIdle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 600)
+                    .offset(y: hamsterOffset) // moves based on state
+            }
+        }
+        .onAppear {
+            withAnimation(.spring(duration: 0.8)) {
+                hamsterOffset = 60 // slides up into view
+            }
+        }
     }
 }
 
 #Preview {
-    SplashScreenView()
-}
+            SplashScreenView()
+        }
